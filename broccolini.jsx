cfe07c3c0c09cdcb4f21865ad2b6ba2a1b177309
Diana Mounter <broccolini@github.com>
2017-12-06 22:13:21 -0500

    start building new site

---
+++ broccolini.jsx
@@ -0,0 +1,81 @@
+<Container ml={[4,6,7]} pt={7} mb={2} bg='white'>
+   <LeadText>
+   <LinkDark href='http://broccolini.net'>
+      broccolini
+      </LinkDark>
+    </LeadText>
+    <Heading1>
+      Diana Mounter
+    </Heading1>
+</Container>
+<Box ml={[4,6,7]} mb={6} bg='yellow'>
+  <Container py={3} pl={3}>
+    <Text mr={1}>
+    I'm a designer with a passion for the web. I specialize in design systems, and have a background in user-centered design. Currently I manage the design systems team at GitHub, and organize the <LinkDark href='http://designsystems.nyc'>NYC chapter of the Design Systems Coalition</LinkDark>. I write and speak about design, code, and working with people.  I live in Brooklyn, NY, and I love it.
+
+    </Text>
+  </Container>
+</Box>
+<Container ml={[4,6,7]}>
+  <Box>
+    <HeadingAccent mb={3}>
+      Writing
+    </HeadingAccent>
+    <Media
+    href='http://broccolini.net/'
+    title='Putting your design system into practice'
+    text='How you develop your design system will influence how you share and encourage adoption of the system.'
+    />
+    <Media
+    href='http://broccolini.net/'
+    title='How to empower designers to code'
+    text='For many designers the best way to learn front-end development is on the job. Many companies want to attract designers who code, but may fall short of providing enough support.'
+    />
+  </Box>
+  <Box pt={5}>
+    <HeadingAccent mb={3}>
+      Talks
+    </HeadingAccent>
+    <Media
+    href='http://broccolini.net/'
+    title='Interaction of Color Systems'
+    text='How you develop your design system will influence how you share and encourage adoption of the system.'
+    />
+    <Media
+    href='http://broccolini.net/'
+    title='Introducing Systems into Chaos'
+    text='For many designers the best way to learn front-end development is on the job. Many companies want to attract designers who code, but may fall short of providing enough support.'
+    />
+  </Box>
+  <Box pt={5}>
+    <HeadingAccent mb={3}>
+      Interviews
+    </HeadingAccent>
+    <Media
+    href='http://broccolini.net/'
+    title='Full Stack Radio interview'
+    text='How you develop your design system will influence how you share and encourage adoption of the system.'
+    />
+    <Media
+    href='http://broccolini.net/'
+    title='UX Pin interview'
+    text='For many designers the best way to learn front-end development is on the job. Many companies want to attract designers who code, but may fall short of providing enough support.'
+    />
+  </Box>
+  <Box pt={5}>
+   
+  </Box>
+</Container>
+<Box ml={[4,6,7]} mb={6} bg='grayLight'>
+  <Container py={3} pl={3}>
+     <Heading mb={3}>
+      Side projets
+    </Heading>
+    <Box display='inlineBlock'>
+      <Image src='https://images.unsplash.com/photo-1484728863103-3f0403e03a7b?auto=format&fit=crop&w=3302&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' />
+      </Box>
+    <Box display='inlineBlock'>
+      <Image src='https://images.unsplash.com/photo-1484728863103-3f0403e03a7b?auto=format&fit=crop&w=3302&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' />
+      </Box>
+  </Container>
+</Box>
