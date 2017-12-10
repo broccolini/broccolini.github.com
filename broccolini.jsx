afd580f4a81d0b140b88a8dbc18ef548d130bccf
Diana Mounter <broccolini@github.com>
2017-12-10 16:30:28 -0500

    grid layout

--- broccolini.jsx
+++ broccolini.jsx
@@ -1,4 +1,6 @@
-<Container ml={[4,6,7]} pt={7} mb={2} bg='white'>
+<Box ml={['10%','0']}>
+<Container mt={[4,6,'20%']} mb={2}>
+  <Box width={[1,1,3/4,2/3]} pr={3}>
    <LeadText>
    <LinkDark href='http://broccolini.net'>
       broccolini
@@ -7,75 +9,195 @@
     <Heading1>
       Diana Mounter
     </Heading1>
+  </Box>
 </Container>
-<Box ml={[4,6,7]} mb={6} bg='yellow'>
-  <Container py={3} pl={3}>
-    <Text mr={1}>
-    I'm a designer with a passion for the web. I specialize in design systems, and have a background in user-centered design. Currently I manage the design systems team at GitHub, and organize the <LinkDark href='http://designsystems.nyc'>NYC chapter of the Design Systems Coalition</LinkDark>. I write and speak about design, code, and working with people.  I live in Brooklyn, NY, and I love it.
-
-    </Text>
+<Flex>
+  <Container mb={7} bg='yellow'>
+    <Box width={[1,1,1,2/3]}>
+      <Text p={3} pr={3,2,0}>
+      I'm a designer with a passion for the web. I specialize in design systems, and have a background in user-centered design. Currently I manage the design systems team at GitHub, and organize the <LinkDark href='http://designsystems.nyc'>NYC chapter of the Design Systems Coalition</LinkDark>. I write and speak about design, code, and working with people.  I live in Brooklyn, NY, and I love it.
+      </Text>
+    </Box>
   </Container>
-</Box>
-<Container ml={[4,6,7]}>
-  <Box>
-    <HeadingAccent mb={3}>
+</Flex>
+<Flex>
+  <Container mb={5}>
+    <HeadingAccent mb={4} ml={2}>
       Writing
     </HeadingAccent>
+    <Box width={[1,1,1,2/3]} px={2}>
     <Media
     href='http://broccolini.net/'
-    title='Putting your design system into practice'
-    text='How you develop your design system will influence how you share and encourage adoption of the system.'
-    />
+    title='Design systems handbook: putting your design system into practice'
+    text='This chapter focusses on how to encourage adoption of your design system, publishing and distribution methods, and considerations for making your design system public vs private.'/>
     <Media
     href='http://broccolini.net/'
     title='How to empower designers to code'
-    text='For many designers the best way to learn front-end development is on the job. Many companies want to attract designers who code, but may fall short of providing enough support.'
-    />
-  </Box>
-  <Box pt={5}>
-    <HeadingAccent mb={3}>
+    text='For many designers the best way to learn front-end development is on the job. Many companies want to attract designers who code, but may fall short of providing enough support.'/>
+    <Media
+    href='https://codeascraft.com/2015/02/05/rebuilding-the-foundation-of-etsy-seller-tools/'
+    title='Rebuilding the Foundation of Etsy&#8217;s Seller Tools'
+    text='How we redesigned the seller-facing web app, and built a new style guide in the process. Written for Etsy&#8217;s Code as Craft blog, in collaboration with coworkers from the shop management team.'/>
+    <Media
+    href='https://medium.com/@broccolini/embracing-change-in-your-workplace-544ec73db0c8'
+    title='Embracing change in your workplace'
+    text='The biggest challenges you face in a workplace are often not in how to build the product but in how to build a healthy work environment.'/>
+    <Media
+    href='https://medium.com/@broccolini/embracing-change-in-your-workplace-544ec73db0c8'
+    title='Move fast and make something &#40;of your life&#41;'
+    text='I spent the majority of my first trip to America looking at a computer screen, while sat on a bus that traveled from San Francisco to Austin. It turned out to be a life-changing experience.'/>
+    </Box>
+  </Container>
+</Flex>
+<Flex>
+  <Container mb={5}>
+    <HeadingAccent mb={4} ml={2}>
+      Proud of
+    </HeadingAccent>
+    <Box width={[1,1,3/4,2/3]} px={2}>
+      <Box width={[1,1/2]} py={4} pr={1} bg='black' height='100%'>
+      hi
+      </Box>
+    </Box>
+  </Container>
+</Flex>
+<Flex>
+  <Container mb={5}>
+    <HeadingAccent mb={4} ml={2}>
       Talks
     </HeadingAccent>
-    <Media
-    href='http://broccolini.net/'
-    title='Interaction of Color Systems'
-    text='How you develop your design system will influence how you share and encourage adoption of the system.'
-    />
-    <Media
-    href='http://broccolini.net/'
-    title='Introducing Systems into Chaos'
-    text='For many designers the best way to learn front-end development is on the job. Many companies want to attract designers who code, but may fall short of providing enough support.'
-    />
-  </Box>
-  <Box pt={5}>
-    <HeadingAccent mb={3}>
+    <Box width={[1,1,3/4,2/3]} px={2}>
+      <Media
+      href='http://broccolini.net/'
+      title='Interaction of Color Systems'
+      text='Clarity, November 2017'
+      />
+      <Media
+      href='https://youtu.be/FZSi1bK-BRM'
+      title='Introducing Systems into Chaos'
+      text='RETHINK Product Design Series, August 2017'
+      />
+      <Media
+      href='https://youtu.be/Hx02SaL_IH0'
+      title='Design systems: when and how much?'
+      text='Active Ingredients, April 2017'/>
+      <Media
+      href='http://firstmarkcap.com/insights/color-systems-at-github/'
+      title='Color Systems'
+      text='Design Driven, March 2017'/>
+      <Media
+      href='http://aigany.org/event/real-talk-about-design-systems/'
+      title='Panel: Real talk about design systems'
+      text='AIGA, February 2017'/>
+      <Media
+      href='http://aigany.org/event/real-talk-about-design-systems/'
+      title='Building design systems at GitHub'
+      text='NYC Design Systems Coalition, December 2016'/>
+      <Media
+      href='https://speakerdeck.com/broccolini/competence-compassion-and-confidence'
+      title='Competence, Compassion, and Confidence'
+      text='Wildhacks, November 2016'/>
+      <Media
+      href='https://speakerdeck.com/broccolini/empowering-designers-to-code-2'
+      title='Empowering designers to code - 2nd edition'
+      text='Awwwards, November 2016'/>
+      <Media
+      href='http://dwc.io/events/2016/design-night-at-mongodb-hq/'
+      title='How to build style guides everyone loves!'
+      text='Designers Who Code, June 2016'/>
+      <Media
+      href='https://speakerdeck.com/broccolini/empowering-designers-to-code-2'
+      title='All the things I know about design systems'
+      text='Clarity, March 2016'/>
+      <Media
+      href='https://speakerdeck.com/broccolini/empowering-designers-to-code-2'
+      title='Empowering designers to code'
+      text='FOWD, November 2015'/>
+      <Media
+      href='http://slides.com/dianamounter/building-scalable-design#/'
+      title='Building scalable design'
+      text='Design Driven, November 2014'/>
+      <Media
+      href='http://www.thisismetis.com/blog/design-for-people'
+      title='Design for people'
+      text='Metis, November 2014'/>
+      <Media
+      href='http://www.thisismetis.com/blog/design-for-people'
+      title='Embracing change in the workplace'
+      text='Agile Asbury, October 2014'/>
+    </Box>
+  </Container>
+</Flex>
+<Box mb={5}>
+  <Container>
+    <HeadingAccent mb={4} ml={2}>
+      Side projects
+    </HeadingAccent>
+  </Container>
+    <Flex mb={3} ml={['10%','25%', 0]} wrap>
+      <Box width={[1,3/4,1/4,1/4]} px={2}>
+      </Box>
+      <Box width={[1,3/4,1/4,1/4]} px={2} mb={3,3,4}>
+        <Card
+        src='images/50-shades.png'
+        href='http://broccolini.net/50shadesofblk/'
+        title='50 Shades of black'
+        />
+      </Box>
+      <Box width={[1,3/4,1/4,1/4]} px={2} mb={3,3,4}>
+        <Card
+        src='images/wkyuan.png'
+        href='http://whatkeepsyouupatnight.io/'
+        title='What keeps you up at night?'
+        />
+      </Box>
+      <Box width={[1,3/4,1/4,1/4]} px={2} mb={3,3,4}>
+        <Card
+        src='images/swiss.png'
+        href='http://broccolini.net/swiss/'
+        title='Swiss Jekyll theme'
+        />
+      </Box>
+      <Box width={[1,3/4,1/4,1/4]} px={2} mb={3,3,4}>
+        <Card
+        src='images/athena.png'
+        href='http://broccolini.net/athena/'
+        title='Athena Jekyll theme'
+        />
+      </Box>
+      <Box width={[1,3/4,1/4,1/4]} px={2} mb={3,3,4}>
+        <Card
+        src='images/soundsradcakes.png'
+        href='http://soundsradcakes.com/'
+        title='Soundsradcakes'
+        />
+      </Box>
+      <Box width={[1,3/4,1/4,1/4]} px={2} mb={3,3,4}>
+        <Card
+        src='images/robotodex.png'
+        href='http://robotodex.com/'
+        title='Robotodex'
+        />
+      </Box>
+      <Box width={[1,2/5,1/4]} px={2}>
+      </Box>
+    </Flex>
+</Box>
+<Flex>
+  <Container mb={5}>
+    <HeadingAccent mb={4} ml={2}>
       Interviews
     </HeadingAccent>
+    <Box width={3/4} px={2}>
     <Media
     href='http://broccolini.net/'
     title='Full Stack Radio interview'
-    text='How you develop your design system will influence how you share and encourage adoption of the system.'
-    />
+    text='How you develop your design system will influence how you share and encourage adoption of the system.'/>
     <Media
     href='http://broccolini.net/'
-    title='UX Pin interview'
-    text='For many designers the best way to learn front-end development is on the job. Many companies want to attract designers who code, but may fall short of providing enough support.'
-    />
-  </Box>
-  <Box pt={5}>
-   
-  </Box>
-</Container>
-<Box ml={[4,6,7]} mb={6} bg='grayLight'>
-  <Container py={3} pl={3}>
-     <Heading mb={3}>
-      Side projets
-    </Heading>
-    <Box display='inlineBlock'>
-      <Image src='https://images.unsplash.com/photo-1484728863103-3f0403e03a7b?auto=format&fit=crop&w=3302&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' />
-      </Box>
-    <Box display='inlineBlock'>
-      <Image src='https://images.unsplash.com/photo-1484728863103-3f0403e03a7b?auto=format&fit=crop&w=3302&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D' />
-      </Box>
+    title='Design Details'
+    text='For many designers the best way to learn front-end development is on the job. Many companies want to attract designers who code, but may fall short of providing enough support.'/>
+    </Box>
   </Container>
-</Box>
+  </Flex>
+  </Box>
