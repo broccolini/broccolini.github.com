bce150435eef0f329c517dd36daddf86a37749ab
Diana Mounter <broccolini@github.com>
2017-12-30 17:11:43 -0500

    many updates: - added nav links - fixed up highlights sections - added footer - tweaked side projects layout

--- broccolini.jsx
+++ broccolini.jsx
@@ -1,7 +1,8 @@
 <meta name='viewport' content='width=device-width,initial-scale=1' />
+
 <Box ml={['10%','0']}>
-<Container mt={[4,6,6,'20%']} mb={2}>
-  <Box width={[1,1,2/3,3/4]} pr={3}>
+<Container mt={[5,6,6,'20%']} mb={2}>
+  <Box width={[1,1,1,2/3]} pl={2} pr={3}>
    <LeadText>
    <LinkDark href='http://broccolini.net'>
       broccolini
@@ -12,119 +13,163 @@
     </Heading1>
   </Box>
 </Container>
-<Flex>
-  <Container mb={7} bg='yellow'>
+<Box ml={2}>
+  <Container mb={[5,6,7]} bg='yellow'>
     <Box width={[1,1,1,2/3]}>
-      <Text p={3} pr={2,2,3,3}>
+      <Text py={3} pl={3} pr={[2,2,3,3]}>
       I'm a designer with a passion for the web. I specialize in design systems, and have a background in user-centered design. Currently I manage the design systems team at GitHub, and organize the <LinkDark href='http://designsystems.nyc'>NYC chapter of the Design Systems Coalition</LinkDark>. I write and speak about design, code, and working with people.  I live in Brooklyn, NY, and I love it.
       </Text>
     </Box>
   </Container>
-</Flex>
+</Box>
+<Container mb={6}>
+<Box py={2} ml={2}>
+<Nav>
+  <NavList width={1/5}
+    href='#writing'
+    title='Writing'
+  />
+  <NavList
+    href='#proud-of'
+    title='Hightlights'
+  />
+  <NavList
+    href='#talks'
+    title='Talks'
+  />
+  <NavList
+    href='#side-projects'
+    title='Side projects'
+  />
+  <NavList
+    href='#interviews'
+    title='Interviews'
+  />
+</Nav>
+ </Box>
+ </Container>
 <Flex>
-  <Container mb={5}>
+  <Container mb={4}>
     <HeadingAccent mb={4} ml={2}>
       Writing
     </HeadingAccent>
     <Box width={[1,1,1,2/3]} px={2}>
     <Media
-    href='http://broccolini.net/'
-    title='Design systems handbook: putting your design system into practice'
+    href='https://www.designbetter.co/design-systems-handbook/putting-design-system-practice'
+    title='Design Systems Handbook: putting your design system into practice'
     text='This chapter focusses on how to encourage adoption of your design system, publishing and distribution methods, and considerations for making your design system public vs private.'/>
     <Media
-    href='http://broccolini.net/'
-    title='How to empower designers to code'
+    href='http://www.creativebloq.com/web-design/empower-designers-code-41619919'
+    title='How to Empower Designers to Code'
     text='For many designers the best way to learn front-end development is on the job. Many companies want to attract designers who code, but may fall short of providing enough support.'/>
     <Media
     href='https://codeascraft.com/2015/02/05/rebuilding-the-foundation-of-etsy-seller-tools/'
-    title='Rebuilding the Foundation of Etsy&#8217;s Seller Tools'
+    title='Rebuilding the Foundation of Etsy’s Seller Tools'
     text='How we redesigned the seller-facing web app, and built a new style guide in the process. Written for Etsy&#8217;s Code as Craft blog, in collaboration with coworkers from the shop management team.'/>
     <Media
     href='https://medium.com/@broccolini/embracing-change-in-your-workplace-544ec73db0c8'
-    title='Embracing change in your workplace'
+    title='Embracing Change in Your Workplace'
     text='The biggest challenges you face in a workplace are often not in how to build the product but in how to build a healthy work environment.'/>
     <Media
     href='https://medium.com/@broccolini/embracing-change-in-your-workplace-544ec73db0c8'
-    title='Move fast and make something &#40;of your life&#41;'
+    title='Move Fast and Make Something (of your life)'
     text='I spent the majority of my first trip to America looking at a computer screen, while sat on a bus that traveled from San Francisco to Austin. It turned out to be a life-changing experience.'/>
     </Box>
   </Container>
 </Flex>
-<Flex>
-  <Container mb={5}>
+
+<Box mb={5}>
+  <Container>
     <HeadingAccent mb={4} ml={2}>
-      Proud of
+      Highlights
     </HeadingAccent>
-    <Box width={[1,1,3/4,2/3]} px={2}>
-      <Box width={[1,1/2]} py={4} pr={1} bg='black' height='100%'>
-      hi
+
+    <Flex mb={3}wrap>
+      <Box width={[1,3/4,1/2,1/3]} px={2} mb={3,3,4}>
+        <Card
+        src='images/dsc-yellow.png'
+        href='http://designsystems.nyc/'
+        title='Design Systems Coalition NYC'
+        />
+        <Text mt={1}>The DSC is a community for people who are passionate about design systems to share and learn from each other. I organize the NYC chapter which I formed in December 2016.
+        </Text>
       </Box>
-    </Box>
-  </Container>
-</Flex>
+      <Box width={[1,3/4,1/2,1/3]} px={2} mb={3,3,4}>
+        <Card
+        src='images/primer.png'
+        href='http://primer.github.io/'
+        title='Primer design system'
+        />
+        <Text mt={1}>I helped form the GitHub design systems team in early 2016. I contribute to and guide the evolution of Primer, our (proudly) open-sourced design system.
+        </Text>
+      </Box>
+    </Flex>
+    </Container>
+</Box>
+
 <Flex>
   <Container mb={5}>
     <HeadingAccent mb={4} ml={2}>
       Talks
     </HeadingAccent>
     <Box width={[1,1,3/4,2/3]} px={2}>
-      <Media
-      href='http://broccolini.net/'
+      <MediaSmall
+      href='https://www.clarityconf.com/the-interaction-of-color-systems/'
       title='Interaction of Color Systems'
       text='Clarity, November 2017'
       />
-      <Media
+      <MediaSmall
       href='https://youtu.be/FZSi1bK-BRM'
       title='Introducing Systems into Chaos'
       text='RETHINK Product Design Series, August 2017'
       />
-      <Media
+      <MediaSmall
       href='https://youtu.be/Hx02SaL_IH0'
-      title='Design systems: when and how much?'
+      title='Design Systems: when and how much?'
       text='Active Ingredients, April 2017'/>
-      <Media
+      <MediaSmall
       href='http://firstmarkcap.com/insights/color-systems-at-github/'
       title='Color Systems'
       text='Design Driven, March 2017'/>
-      <Media
+      <MediaSmall
       href='http://aigany.org/event/real-talk-about-design-systems/'
-      title='Panel: Real talk about design systems'
+      title='Panel: Real Talk About Design Systems'
       text='AIGA, February 2017'/>
-      <Media
-      href='http://aigany.org/event/real-talk-about-design-systems/'
-      title='Building design systems at GitHub'
+      <MediaSmall
+      href='https://speakerdeck.com/broccolini/building-design-systems-at-github'
+      title='Building Design Systems at GitHub'
       text='NYC Design Systems Coalition, December 2016'/>
-      <Media
+      <MediaSmall
       href='https://speakerdeck.com/broccolini/competence-compassion-and-confidence'
       title='Competence, Compassion, and Confidence'
       text='Wildhacks, November 2016'/>
-      <Media
+      <MediaSmall
       href='https://speakerdeck.com/broccolini/empowering-designers-to-code-2'
-      title='Empowering designers to code - 2nd edition'
+      title='Empowering Designers to Code - 2nd edition'
       text='Awwwards, November 2016'/>
-      <Media
+      <MediaSmall
       href='http://dwc.io/events/2016/design-night-at-mongodb-hq/'
-      title='How to build style guides everyone loves!'
+      title='How to Build Style Guides Everyone Loves'
       text='Designers Who Code, June 2016'/>
-      <Media
-      href='https://speakerdeck.com/broccolini/empowering-designers-to-code-2'
-      title='All the things I know about design systems'
+      <MediaSmall
+      href='https://speakerdeck.com/broccolini/all-the-things-i-know-about-building-design-systems'
+      title='All The Things I Know About Design Systems'
       text='Clarity, March 2016'/>
-      <Media
-      href='https://speakerdeck.com/broccolini/empowering-designers-to-code-2'
-      title='Empowering designers to code'
+      <MediaSmall
+      href='https://speakerdeck.com/broccolini/empowering-designers-to-code'
+      title='Empowering Designers to Code'
       text='FOWD, November 2015'/>
-      <Media
+      <MediaSmall
       href='http://slides.com/dianamounter/building-scalable-design#/'
-      title='Building scalable design'
+      title='Building Scalable Design'
       text='Design Driven, November 2014'/>
-      <Media
+      <MediaSmall
       href='http://www.thisismetis.com/blog/design-for-people'
-      title='Design for people'
+      title='Design for People'
       text='Metis, November 2014'/>
-      <Media
-      href='http://www.thisismetis.com/blog/design-for-people'
-      title='Embracing change in the workplace'
+      <MediaSmall
+      href='https://speakerdeck.com/broccolini/responding-to-change'
+      title='Responding to Change'
       text='Agile Asbury, October 2014'/>
     </Box>
   </Container>
@@ -135,7 +180,7 @@
       Side projects
     </HeadingAccent>
   </Container>
-    <Flex mb={3} ml={['10%','25%', 0]} wrap>
+    <Flex mb={3} ml={['0','20%','0','0']} wrap>
       <Box width={[1,3/4,1/3,1/4]} px={2}>
       </Box>
       <Box width={[1,3/4,1/3,1/4]} px={2} mb={3,3,4}>
@@ -189,14 +234,29 @@
     </HeadingAccent>
     <Box width={3/4} px={2}>
     <Media
-    href='http://broccolini.net/'
-    title='Full Stack Radio interview'
-    text='How you develop your design system will influence how you share and encourage adoption of the system.'/>
+    href='http://spec.fm/podcasts/design-details/19431'
+    title='Design Details, episode 74'
+    text='Hear me talk about my journey to Etsy, the startup bus, why Git is a valuable tool for designers, and onboarding designers at Etsy.'/>
     <Media
-    href='http://broccolini.net/'
-    title='Design Details'
-    text='For many designers the best way to learn front-end development is on the job. Many companies want to attract designers who code, but may fall short of providing enough support.'/>
+    href='http://www.fullstackradio.com/75'
+    title='Full Stack Radio, episode 75'
+    text='In this interview I talk about using design systems to tame legacy CSS, and how GitHub is using utility classes to make it easier to build more consistent, maintainable user interfaces.'/>
+    <Media
+    href='https://www.uxpin.com/studio/blog/githubs-design-system-interview-diana-mounter/'
+    title='UXPin interview'
+    text='In this interview I talk about lessons learned in building and maintaining GitHub’s design system, Primer.'/>
     </Box>
   </Container>
   </Flex>
+  </Box>
+  <Box bg='grayLight'>
+    <Container>
+      <Box ml={['10%','0']}>
+        <Text py={3} pl={2} pr={[2,2,3,3]}>
+        <LinkDark href='https://twitter.com/broccolini' mr={2}>Twitter</LinkDark> 
+        <LinkDark href='https://github.com/broccolini' mr={2}>GitHub</LinkDark> 
+        <LinkDark href='https://medium.com/@broccolini' mr={2}>Medium</LinkDark>
+        </Text>
+      </Box>
+    </Container>
   </Box>
